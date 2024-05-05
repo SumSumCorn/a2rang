87b5153e9fc5cc504427b83fe8b1bea8ff64e4e9
SumSumCorn <a01035587180@gmail.com>
2024-05-05 14:26:19 +0900

    add: category button

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,40 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import React, { useState } from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import CategoryBoxes from '@/components/CategoryBoxes';
 
 export default function Page() {
+  const [query, setQuery] = useState('');
+  const handleSearch = (text: string) => {
+    // const formattedQuery = text.toLowerCase();
+    // const filteredData = data.filter(item => item.toLowerCase().includes(formattedQuery));
+    setQuery(text);
+    // setData(filteredData);
+  };
   return (
     <View style={styles.container}>
+      {/* 검색 컴포넌트 */}
+      <View style={styles.searchSection}>
+        <TextInput
+          style={styles.input}
+          value={query}
+          onChangeText={handleSearch}
+          placeholder='검색어를 입력하세요'
+          placeholderTextColor='#999'
+        />
+        <TouchableOpacity onPress={() => handleSearch(query)}>
+          <AntDesign name='search1' size={24} color='#999' />
+          {/* <Icon name="search" size={20} color="#999" style={styles.searchIcon} /> */}
+        </TouchableOpacity>
+      </View>
+      {/* box컴포넌트 */}
+      <CategoryBoxes />
+      {/* 핫한행사컴포넌트 */}
+      {/* 아이랑추천픽컴포넌트 */}
+      {/* 최근등록된행사 컴포넌트 */}
       <Text style={styles.title}>Tab One 안녕하세요 아이랑입니다</Text>
       <View
         style={styles.separator}
@@ -21,7 +50,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 20,
@@ -31,5 +61,29 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  searchSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    marginTop: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+  },
+  input: {
+    flex: 1,
+    // padding: 10,
+    fontSize: 16,
+    color: '#424242',
+    backgroundColor: '#f0f0f0',
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
---
+++ components/CategoryBoxes.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Colors from '@/constants/Colors';
+import { useAssets } from 'expo-asset';
+
+const categories = [
+  { id: '1', name: '전시·미술', icon: '@/assets/images/icon/art' },
+  { id: '2', name: '공연', icon: '@/assets/images/icon/mic' },
+  { id: '3', name: '교육·체험', icon: '@/assets/images/icon/education' },
+  { id: '4', name: '축제', icon: '@/assets/images/icon/festival' },
+  { id: '5', name: '영화', icon: '@/assets/images/icon/film' },
+  { id: '6', name: '기타', icon: '@/assets/images/icon/etc' },
+];
+const CategoryBoxes = () => {
+  const [assets, error] = useAssets([
+    require('@/assets/images/icon/art.png'),
+    require('@/assets/images/icon/mic.png'),
+    require('@/assets/images/icon/education.png'),
+    require('@/assets/images/icon/festival.png'),
+    require('@/assets/images/icon/film.png'),
+    require('@/assets/images/icon/etc.png'),
+  ]);
+
+  return (
+    <View style={styles.container}>
+      {categories.map((category) => (
+        <TouchableOpacity key={category.id} style={styles.button}>
+          {assets ? <Image source={assets[+category.id - 1]} /> : null}
+          <Text style={styles.label}>{category.name}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    marginHorizontal: 10,
+  },
+  button: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    backgroundColor: '#fff', // Background color for the buttons
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 3,
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
+export default CategoryBoxes;
