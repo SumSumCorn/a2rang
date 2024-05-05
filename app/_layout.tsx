9a8fabfa9d1363958c441c280c2ad0f15bcff2aa
SumSumCorn <a01035587180@gmail.com>
2024-05-05 19:51:56 +0900

    stlye: apply font PretendardVariable

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,66 +2,39 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 // import { useFonts } from 'expo-font';
 
-import {
-  useFonts,
-  NotoSansKR_100Thin,
-  NotoSansKR_300Light,
-  NotoSansKR_400Regular,
-  NotoSansKR_500Medium,
-  NotoSansKR_700Bold,
-  NotoSansKR_900Black,
-} from '@expo-google-fonts/noto-sans-kr';
 import { Ionicons } from '@expo/vector-icons';
 import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
 import { TouchableOpacity } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useFonts } from 'expo-font';
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    PretendardVariable: require('../assets/fonts/PretendardVariable.ttf'),
     ...FontAwesome.font,
   });
 
-  let [fontsLoaded] = useFonts({
-    NotoSansKR_100Thin,
-    NotoSansKR_300Light,
-    NotoSansKR_400Regular,
-    NotoSansKR_500Medium,
-    NotoSansKR_700Bold,
-    NotoSansKR_900Black,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
 
   useEffect(() => {
-    if (!fontsLoaded) {
+    if (loaded) {
       SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
+  }, [loaded]);
 
   return <RootLayoutNav />;
 }
@@ -80,6 +53,22 @@ function RootLayoutNav() {
             name='festival'
             options={{
               title: '행사',
+              headerLeft: () => (
+                <TouchableOpacity onPress={router.back}>
+                  <Ionicons name='arrow-back' size={24} />
+                </TouchableOpacity>
+              ),
+              headerRight: () => (
+                <TouchableOpacity onPress={router.back}>
+                  <Ionicons name='search' size={24} />
+                </TouchableOpacity>
+              ),
+            }}
+          />
+          <Stack.Screen
+            name='detail/[id]'
+            options={{
+              title: '상세정보',
               headerLeft: () => (
                 <TouchableOpacity onPress={router.back}>
                   <Ionicons name='arrow-back' size={24} />
